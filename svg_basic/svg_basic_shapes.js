cb2878a566c202a443966681ff518d6399024e1c
Robin Soubry <robin.soubry@gmail.com>
2016-06-21 08:15:11 -0700

    Add green circle

--- svg_basic/svg_basic_shapes.js
+++ svg_basic/svg_basic_shapes.js
@@ -5,4 +5,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
                   .attr("width",500)
                   .attr("height",500)
 
+  // Step 2: Add a circle to the canvas
+  var circle = canvas.append("circle")
+                      .attr("cx",250) // c = center | x = horizontal axis => Similar to margin in css
+                      .attr("cy",250)
+                      .attr("r",50) // Give the circle a radius
+                      .attr("fill","green") // Fill color of the object is an attribute, not a style
+
+
 });
