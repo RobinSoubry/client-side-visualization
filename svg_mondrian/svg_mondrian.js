5aabbd5b4312b931a35d184cd0bbf6f7936ad6ea
Robin Soubry <robin.soubry@gmail.com>
2016-06-21 08:07:28 -0700

    Add svg canvas

--- svg_mondrian/svg_mondrian.js
+++ svg_mondrian/svg_mondrian.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(e) { 
+  // Step 1: Create a canvas to draw the svg on.
+  var canvas = d3.select('#mondrian') // We use the #mondrian as a selector, to easily center the div on page.
+                  .append('svg')
+                  .attr('width',700)
+                  .attr('height',800);
 
 });
