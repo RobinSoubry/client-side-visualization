880b789c3761e601753cdcf7d36b491014b6e4bb
Robin Soubry <robin.soubry@gmail.com>
2016-06-21 13:51:58 -0700

    Add bars and source data set

--- d3_bar_chart/d3_bar_chart.js
+++ d3_bar_chart/d3_bar_chart.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
 
+  var dataArray = [20, 40, 50, 60];
   var width = 500;
   var height = 500;
 
@@ -7,5 +8,14 @@ document.addEventListener("DOMContentLoaded", function(e) {
               .append('svg')
               .attr('width',width)
               .attr('height',height);
+
+  var bars = canvas.selectAll('rect')
+                  .data(dataArray)
+                  .enter()
+                      .append('rect')
+                      .attr('width', function(d) { return widthScale(d); })
+                      .attr('height', 50)
+                      .attr('fill', 'blue')
+                      .attr('y', function(d,i) { return i *100 });
 
 });
