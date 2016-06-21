9258fcba106c211fa8a7b64226245fc2e0672966
Robin Soubry <robin.soubry@gmail.com>
2016-06-21 13:51:18 -0700

    Add svg canvas

--- d3_bar_chart/d3_bar_chart.js
+++ d3_bar_chart/d3_bar_chart.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
 
+  var width = 500;
+  var height = 500;
+
+  var canvas = d3.select('body')
+              .append('svg')
+              .attr('width',width)
+              .attr('height',height);
+
 });
