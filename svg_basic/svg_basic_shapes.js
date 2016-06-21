8f185541c042f236f56f0de610b0ddc15ee6542e
Robin Soubry <robin.soubry@gmail.com>
2016-06-21 08:14:48 -0700

    Add svg canvas

--- svg_basic/svg_basic_shapes.js
+++ svg_basic/svg_basic_shapes.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
-  
+// Step 1: Create a canvas to draw the svg on.
+  var canvas = d3.select("body") // We use the body as a selector, but virtually any div can be used.
+                  .append("svg")
+                  .attr("width",500)
+                  .attr("height",500)
 
 });
